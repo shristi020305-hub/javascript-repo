f9fc034c49ea9efe8d9a41cc7d1e3f0a4a4a95d2
shristi020305-hub <shristi020305@gmail.com>
2026-06-14 21:12:46 +0530

    anonymous

---
+++ anonymousfun.js
@@ -0,0 +1,13 @@
+//normal funtion
+const greet= function(){
+console.log("Hello! \u{1F64F}");
+
+}
+greet();
+//Anonymous function->function without name.
+setTimeout(function(){
+    console.log("======through Anonymous======");
+    console.log("\u{2764}\u{FE0F}");
+    console.log("Hello! \u{1F64F}");
+
+},5000)
