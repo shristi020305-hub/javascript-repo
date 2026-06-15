32b41c4253dd7794e7de417fd1f469ab7b155b5a
shristi020305-hub <shristi020305@gmail.com>
2026-06-15 17:26:25 +0530

    anonymous with a simple task

--- anonymousfun.js
+++ anonymousfun.js
@@ -1,13 +1,27 @@
-//normal funtion
-const greet= function(){
-console.log("Hello! \u{1F64F}");
+// //normal funtion
+// const greet= function(){
+// console.log("Hello! \u{1F64F}");
 
+// }
+// greet();
+// //Anonymous function->function without name.
+// setTimeout(function(){
+//     console.log("======through Anonymous======");
+//     console.log("\u{2764}\u{FE0F}");
+//     console.log("Hello! \u{1F64F}");
+
+// },5000)
+
+
+//printing with 1sec of gaps.
+const greet=function(){
+for(let i=1;i<=5;i++){
+
+setTimeout(() => {
+    console.log(i);
+},i*1000);
+}
+ console.log("\u{2764}\u{FE0F}");
+ console.log("Hello! \u{1F64F}");
 }
 greet();
-//Anonymous function->function without name.
-setTimeout(function(){
-    console.log("======through Anonymous======");
-    console.log("\u{2764}\u{FE0F}");
-    console.log("Hello! \u{1F64F}");
-
-},5000)
