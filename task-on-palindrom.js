acd880841354886351e0c025b10441f3edd23b16
shristi020305-hub <shristi020305@gmail.com>
2026-06-23 14:17:05 +0530

    task on palindrom

---
+++ task-on-palindrom.js
@@ -0,0 +1,35 @@
+//===palindrom===
+//basic.
+// let word="nitin";
+// let arr=word.split("")
+// let reverse=arr.reverse();
+// if(arr==reverse){
+//     console.log(arr.join(""),"-> is Palindrom");
+// }
+// else{
+//     console.log(arr.join(""),"-> is not Palindrom");
+// }
+
+
+
+// const read = require("readline-sync");
+
+// //function palindrom(word){
+// let word=read.question("type word:");
+// word=word.toLowerCase();
+// let arr=word.split("");
+// let reversearr=arr.reverse();
+// let reverseword=reversearr.join("");
+// if(word==reverseword){
+//     console.log(word,"is Palindrom");
+// }
+// else{
+//     console.log(word,"is not Palindrom");
+// }
+// // }
+// //  let input=read.question("type word:");
+// //  palindrom(input);
+
+
+
+
